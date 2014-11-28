57122b65bd858afcc12cd49a6c6e3af6c883a985
Henning Jacobs <henning@jacobs1.de>
2014-11-28 14:30:55 +0100

    add logging

--- src/clojure_web_hello_world/core.clj
+++ src/clojure_web_hello_world/core.clj
@@ -1,13 +1,18 @@
 (ns clojure-web-hello-world.core
       (:use ring.adapter.jetty)
       (:use clojure.string)
+      (:require [clojure.tools.logging :as log])
       (:gen-class))
 (defn extract-name [uri]
       (replace-first uri "/" ""))
 (defn app [req]
-      {:status 200
-            :headers {"Content-Type" "text/html"}
-            :body    (if (= (:uri req) "/")
-                                         "Hello, World!"
-                                         (str "Hello, " (extract-name (:uri req)) "!"))})
+    {:status 200
+     :headers {"Content-Type" "text/html"}
+     :body    (do
+               (log/info "Request:" (:uri req) "Remote:" (:remote-addr req))
+               (if (= (:uri req) "/")
+               "Hello, World!"
+              (str "Hello, " (extract-name (:uri req)) "!"))
+            )
+     })
 (defn -main [] (run-jetty #'app {:port 8080}))
